fbd7e448e6d9f1e36b42d581de1bdaf29745759e
samashtar <sam.ashtar@flatironschool.com>
2019-03-20 23:23:29 -0400

    launch item

---
+++ src/components/LaunchItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function LaunchItem(props) {
+  console.log(props.launch);
+
+  return <div>test</div>;
+}
--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import LaunchItem from "./LaunchItem";
 
 //below is the query you used to summon data using graph ql
 const LAUNCHES_QUERY = gql`
@@ -17,18 +18,23 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
             if (loading) return <h4>Loading..</h4>;
             if (error) console.log(error);
-            console.log(data);
 
-            return <h1>Test</h1>;
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+              </Fragment>
+            );
           }}
         </Query>
-      </div>
+      </Fragment>
     );
   }
 }
