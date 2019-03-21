23590eef9a49c36918e9972317dfcff75dae4898
samashtar <sam.ashtar@flatironschool.com>
2019-03-21 00:08:24 -0400

    appolo setup, launch details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
 import "./App.css";
 import logo from "./logo.jpg";
 
@@ -13,14 +15,17 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="container">
-          <img
-            src={logo}
-            alt="SpaceX"
-            style={{ width: 300, display: "block", margin: "auto" }}
-          />
-          <Launches />
-        </div>
+        <Router>
+          <div className="container">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: "block", margin: "auto" }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
--- src/components/LaunchItem.js
+++ src/components/LaunchItem.js
@@ -1,9 +1,10 @@
 import React from "react";
 import classNames from "classnames";
 import Moment from "react-moment";
+import { Link } from "react-router-dom";
 
 export default function LaunchItem({
-  launch: { flght_number, mission_name, launch_date_local, launch_success }
+  launch: { flight_number, mission_name, launch_date_local, launch_success }
 }) {
   return (
     <div className="card card-body mb-3">
@@ -26,7 +27,9 @@ export default function LaunchItem({
           </p>
         </div>
         <div className="col-md-3">
-          <button className="btn btn-secondary">Launch Details</button>
+          <Link to={`/launch/${flight_number}`} className="btn btn-secondary">
+            Launch Details
+          </Link>
         </div>
       </div>
     </div>
